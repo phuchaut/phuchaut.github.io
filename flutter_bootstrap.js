c3cefd9ce1b782c3bf960e1cd2cf439420939b35
phuchaut <trinhhau010820@gmail.com>
2025-01-29 08:06:54 +0700

    update: title for platform, languages, frameworks

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1303585308"
+    serviceWorkerVersion: "534190863"
   }
 });
