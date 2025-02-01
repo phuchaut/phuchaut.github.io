d11be8a8e245977884a3def5d4aca3ff59429522
phuchaut <trinhhau010820@gmail.com>
2025-02-01 16:23:29 +0700

    add: social icon button

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "942474916"
+    serviceWorkerVersion: "420000115"
   }
 });
