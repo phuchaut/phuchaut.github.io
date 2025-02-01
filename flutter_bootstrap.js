41af92816568024a4cf5cdae10b0cdf4d29cc30c
phuchaut <trinhhau010820@gmail.com>
2025-02-01 13:18:42 +0700

    update: sliverappbar desktop

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "843172884"
+    serviceWorkerVersion: "557599263"
   }
 });
