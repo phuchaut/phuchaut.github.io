e38d367dce098258ccd7d354f79424490f4666fa
phuchaut <trinhhau010820@gmail.com>
2025-02-02 15:00:37 +0700

    update: send email to trinhhau010820@gmail.com

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2233324063"
+    serviceWorkerVersion: "1505959736"
   }
 });
