987384e2af57d8f67e83783c26c7070630bc43ca
phuchaut <trinhhau010820@gmail.com>
2025-02-02 12:37:11 +0700

    update: view cv

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "897563278"
+    serviceWorkerVersion: "2233324063"
   }
 });
